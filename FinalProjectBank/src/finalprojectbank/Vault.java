e9df09c0068fb6faef55c198dfed20ef5b15c6b0
Ethan Senor <ethan.senor99@gmail.com>
2019-04-27 15:49:35 -0400

    JavaDocs Ethan

--- FinalProjectBank/src/finalprojectbank/Vault.java
+++ FinalProjectBank/src/finalprojectbank/Vault.java
@@ -6,24 +6,46 @@
 package finalprojectbank;
 
 /**
+ * All of the money going into and out of the bank is handled here.
  *
  * @author steven
  */
 public class Vault {
     private double totalCash; 
-    
+    /**
+     * This is the constructor function.
+     * 
+     * @see Vault
+     *
+     */
     public Vault() {
         this.totalCash = 0; 
     }
     
+    
+    /**
+     * Adds cash to vault when deposits are made.
+     * 
+     * @param cash 
+     */
     public void addCashToVault(double cash) {
         this.totalCash += cash; 
     }
     
+    /**
+     * Removes cash when a withdrawal is made.
+     * 
+     * @param cash 
+     */
     public void removeCashFromVault(double cash) {
         this.totalCash -= cash;
     }
-
+    
+    /**
+     * Shows the total amount of cash in the bank
+     * 
+     * @return totalCash
+     */
     public double getTotalCash() {
         return totalCash;
     }
--- FinalProjectBank/src/finalprojectbank/bank.java
+++ FinalProjectBank/src/finalprojectbank/bank.java
@@ -12,15 +12,17 @@ import java.util.Map;
 import java.util.Scanner;
 
 /**
+ * This is the backbone of the program. This contains all the information needed to make the bank.
  *
- * @author ersse
+ * @author Ethan Sheraz Steven
+ * @version 1.00
  */
 public class bank {
     //sheraz and I are going to work with a list cause we dont really know how maps work
     //Map<Integer, Customer> Customers = new HashMap();
     private List<Customer> Customers = new ArrayList();
     private Customer current;
-    private static Integer CUSTOMERS_IN_BANK; 
+    //private static Integer CUSTOMERS_IN_BANK; 
     private Customer c1;
         
     Scanner input = new Scanner(System.in);
@@ -28,8 +30,13 @@ public class bank {
     private Vault v1 = new Vault();
     private double addCash;
     
-    //Customers.get(0) = new Customer("Khan", "123", 50.00, 5.00);
-    
+    /**
+     * This is the constructor function it takes no parameters and manually makes the customers. 
+     * 
+     * @see bank
+     * 
+     * 
+     */
     public bank( ) {
         c1 = new Customer("Khan", "123", 50.00, 5.00, true);
         Customers.add(c1);
@@ -48,6 +55,13 @@ public class bank {
         
     }
     
+    /**
+     * This function handles the choices made in the main terminal.
+     * 
+     * 
+     * 
+     * @param option this is the choice made by the user in the main terminal 
+     */
     public void choice(String option) {
         
         
@@ -77,6 +91,13 @@ public class bank {
         }
     }
     
+    /**
+     * This is the validation for the name and password and sets the current logged in user into current.
+     * 
+     * @param name entered name.
+     * @param password entered password.
+     * @return true if the user is found. False if name and password is not valid. 
+     */
     public Boolean userCheck(String name, String password) {
         for(int i = 0; i < Customers.size(); i++) {
             if(Customers.get(i).getLastName().equals(name) && Customers.get(i).getPassword().equals(password)) {
@@ -88,7 +109,11 @@ public class bank {
         
         return false;
     }
-    
+    /**
+     * Deposits a given amount of money into current users account as decided by the user.
+     * <br>
+     * Also adds the money deposited into the total amount for the bank. 
+     */
     public void deposit() {
         double dep;
         char accChoice = ' ';
@@ -108,7 +133,13 @@ public class bank {
             }
         }
     }
-    
+    /**
+     * Withdrawals a given number from the users account.
+     * <br>
+     * also removes the money from the banks total.
+     * <br>
+     * Validates whether the removal is possible by checking total funds in the given account. 
+     */
     public void withdrawl() {
         boolean passed = false;
         double withdrawl;
@@ -133,20 +164,31 @@ public class bank {
         }
         
     }
-    
+    /**
+     * Shows the given account totals.
+     */
     public void view() {
         System.out.println("Total in Savings: $" + current.getSavingsAccount());
         System.out.println("Total in Checking: $" + current.getCheckingAccount());
     }
     
-    //this will only be usuable by an admin
+    /**
+     * Shows the total amount of money inside the vault.
+     * <br>
+     * This function is only usable by an admin.
+     */
     public void getallCash() {
         if(current.getAdmin() == true)
             System.out.println("The total amount of money in the bank right now is: $" + v1.getTotalCash());
         else
             System.out.println("You are not permited to view this information!");
     }
-
+    
+    /**
+     * Generates what monthly and total payments will be for a specific loan amount.
+     * <br>
+     * initial amount and duration(in months) are entered by the user. 
+     */
     public void loan() { 
 
         double a;            // Amount Requested
@@ -158,7 +200,7 @@ public class bank {
         
         System.out.println("Your Interest Rate is: " + b); 
         
-        System.out.println("Please enter the duration of the loan: "); 
+        System.out.println("Please enter the duration of the loan in months: "); 
         c = input.nextDouble();
 
         
